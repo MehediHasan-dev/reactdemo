ab76c7459c82c97acc361832261ee111cc3321b9
mehedihasan-dev <hasanmehedidev@gmail.com>
2024-11-01 14:31:00 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../index.css'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            Home Page
+        </div>
+    )
+}
+
+export default Home
